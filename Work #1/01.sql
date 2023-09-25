9b98fd25a1b2d1c16569300b073f7757fc3c68d3
MarinaPaley <marina_paley@mail.ru>
2023-09-25 13:46:39 +0300

    The first script #1

---
+++ Work #1/01.sql
@@ -0,0 +1,8 @@
+--SELECT 1 AS Value;
+--Structed Query Langues -> SELECT
+--Data Definitation Langues -> CREATE
+--Data Manipulatin Langues -> INSERT
+
+--To show all fields in the table `bookings`
+SELECT *
+FROM bookings AS b;
